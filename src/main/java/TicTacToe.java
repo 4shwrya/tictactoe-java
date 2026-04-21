375be532ed9be49f157e1d28216f4749dce16641
4shwrya <aishwaryanegi3466@gmail.com>
2026-04-21 15:23:27 +0530

    UC1: Display empty Tic-Tac-Toe board

---
+++ src/main/java/TicTacToe.java
@@ -0,0 +1,33 @@
+/**
+ * TicTacToe
+ * UC1 initializes and displays an empty Tic-Tac-Toe board in a proper
+ * grid format. This use case introduces 2D arrays, nested loops,
+ * and formatted console output.
+ */
+
+public class TicTacToe {
+
+    static char[][] board = new char[3][3];
+
+    public static void main(String[] args) {
+        initializeBoard();
+        printBoard();
+    }
+
+    static void initializeBoard() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                board[row][col] = '-';
+            }
+        }
+    }
+
+    static void printBoard() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                System.out.print(board[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
